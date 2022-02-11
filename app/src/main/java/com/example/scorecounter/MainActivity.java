7dbad0868c4eaf7df9d75eb17e25b5bf5e71f141
Vyacheslav-9999 <73278681+Vyacheslav-9999@users.noreply.github.com>
2022-02-11 09:06:33 +0300

    --

--- app/src/main/java/com/example/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/scorecounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.scorecounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -9,7 +11,8 @@ import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.show_ip);
+        Button b = findViewById(R.id.startServer);
         b.setOnClickListener(this);
         findViewById(R.id.connect).setOnClickListener(this);
     }
@@ -25,14 +28,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.show_ip: {
+            case R.id.startServer: {
                 WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                 manager.setWifiEnabled(true);
                 String ipAddress = Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
-                ((Button) view).setText(ipAddress);
-                String warningNoWifiConnected = "Please check that you are connected to wifi";
-                if(ipAddress.equals("0.0.0.0"))
-                    Toast.makeText(this,warningNoWifiConnected,Toast.LENGTH_LONG).show();
+                TextView ip = findViewById(R.id.server_ip);
+                ip.setVisibility(View.VISIBLE);
+                ip.setText(String.format("Server ip : %s", ipAddress));
+
+                copyToClipboard("ScoreCounter server ip", ipAddress);
+
+                if(ipAddress.equals("0.0.0.0"))showWifiWarning();
                 break;
             }
             case R.id.connect: {
@@ -43,5 +49,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
+    }
+    public void showWifiWarning(){
+        String warningNoWifiConnected = "Please check that you are connected to wifi";
+        Toast.makeText(this,warningNoWifiConnected,Toast.LENGTH_LONG).show();
+    }
+    public void copyToClipboard(String label,String text){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText(label,text);
+        clipboard.setPrimaryClip(clip);
     }
 }
